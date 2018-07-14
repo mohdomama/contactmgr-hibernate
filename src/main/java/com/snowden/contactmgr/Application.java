9f6c1ec3007df7c16a24651a4bbd299f0007e5ea
Mohd Omama <mohd.omama@gmail.com>
2018-07-14 11:53:53 +0530

    Add contact model

--- src/main/java/com/snowden/contactmgr/Application.java
+++ src/main/java/com/snowden/contactmgr/Application.java
@@ -1,8 +1,29 @@
 package com.snowden.contactmgr;
 
+import com.snowden.contactmgr.model.Contact;
+import com.snowden.contactmgr.model.Contact.ContactBuilder;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
 
 public class Application {
     // Hold a reusable reference to a session factory
-    
+   // private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        // Create a StandardServiceRegistory
+        final ServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+        return new MetadataSources(registry).buildMetadata().buildSessionFactory();
+
+
+    }
+
+    public static void main(String[] args) {
+        Contact contact = new ContactBuilder("Mohd", "Omama")
+                .withEmail("ammo@maildrop.cc")
+                .withPhone(9012367951L)
+                .build();
+        System.out.println(contact.toString());
+    }
 }
--- src/main/java/com/snowden/contactmgr/model/Contact.java
+++ src/main/java/com/snowden/contactmgr/model/Contact.java
@@ -28,4 +28,36 @@ public class Contact {
 
     // Default constructor for GPA
     public Contact() {}
+
+    public Contact(ContactBuilder builder) {
+        this.firstName = builder.firstName;
+        this.lastName = builder.lastName;
+        this.email = builder.email;
+        this.phone = builder.phone;
+    }
+
+    public static class ContactBuilder {
+        private String firstName;
+        private String lastName;
+        private String email;
+        private Long phone;
+        public ContactBuilder(String firstName, String lastName) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public ContactBuilder withEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public ContactBuilder withPhone(Long phone) {
+            this.phone = phone;
+            return this;
+        }
+
+        public Contact build() {
+            return new Contact(this);
+        }
+    }
 }
