176248d9bbea60c0edc7f590a8087d474d74c123
Mohd Omama <mohd.omama@gmail.com>
2018-07-14 12:17:58 +0530

    Store data into Db

--- src/main/java/com/snowden/contactmgr/Application.java
+++ src/main/java/com/snowden/contactmgr/Application.java
@@ -2,6 +2,7 @@ package com.snowden.contactmgr;
 
 import com.snowden.contactmgr.model.Contact;
 import com.snowden.contactmgr.model.Contact.ContactBuilder;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
@@ -9,7 +10,7 @@ import org.hibernate.service.ServiceRegistry;
 
 public class Application {
     // Hold a reusable reference to a session factory
-   // private static final SessionFactory sessionFactory = buildSessionFactory();
+   private static final SessionFactory sessionFactory = buildSessionFactory();
 
     private static SessionFactory buildSessionFactory() {
         // Create a StandardServiceRegistory
@@ -24,6 +25,20 @@ public class Application {
                 .withEmail("ammo@maildrop.cc")
                 .withPhone(9012367951L)
                 .build();
-        System.out.println(contact.toString());
+
+        // Open a session
+        Session session = sessionFactory.openSession();
+
+        // Begin a transaction
+        session.beginTransaction();
+
+        // Use the session to save the contact
+        session.save(contact);
+
+        // Commit the transaction
+        session.getTransaction().commit();
+
+        // Close the session
+        session.close();
     }
 }
